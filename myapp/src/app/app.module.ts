af61fedac52b09fc89c9dd8856b6f554a332dcbb
wangjie <502024799@qq.com>
2017-08-04 11:55:08 +0800

    angular

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';//引入@angular/forms库中的formsmodule符号
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule //将引入的符号添加到这个数组
   ],
   providers: [],
   bootstrap: [AppComponent]
