c49ebd78a65515d54d3662886e7ec528ca074148
wangjie <502024799@qq.com>
2017-08-08 18:02:53 +0800

    dashbord

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';//引入@angular/forms库中的formsmodule符号
+
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+import { DashboardComponent }   from './dashboard.component';
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    DashboardComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule //将引入的符号添加到这个数组
+    FormsModule, //将引入的符号添加到这个数组
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
